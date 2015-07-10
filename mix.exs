114dc6f2aa39ba5f7859c9ecc0bbeb8b55a57304
Marcelo Gornstein <marcelog@gmail.com>
2015-07-10 16:47:46 -0300

    using new ex_abnf version

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule RFC3966.Mixfile do
 
   defp deps do
     [
-      {:ex_abnf, "~> 0.1.3"},
+      {:ex_abnf, "~> 0.1.4"},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.7", only: :dev}
     ]
